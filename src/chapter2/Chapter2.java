8ebee9a64a3c271de64a06389ab802bcf93e65b7
rkhatchadourian <rkhatchadourian@CST_N417_25.cst.citytech.cuny.edu>
2015-02-11 17:17:44 -0500

    Finished my diamond.

--- src/chapter2/Chapter2.java
+++ src/chapter2/Chapter2.java
@@ -17,6 +17,9 @@ public class Chapter2 {
      */
     public static void main(String[] args) {
         System.out.println("   *\n  ***\n *****\n*******");
+        System.out.println(" *****");
+        System.out.println("  ***");
+        System.out.println("   *");
     }
     
 }
